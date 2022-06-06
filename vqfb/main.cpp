4cf9fdb6bb88058bdb5b342df7516f049a882bb9
Matt Slevinsky <maslevin@gmail.com>
2022-06-06 09:41:59 -0700

    feature: quick and dirty fire effect.

--- vqfb/main.cpp
+++ vqfb/main.cpp
@@ -6,6 +6,7 @@ Author: Matt Slevinsky
 ------------------------------------------------------------*/
 
 #include <kos.h>
+#include <math.h>
 #include "simplexnoise.h"
 
 pvr_init_params_t pvr_params =  {
@@ -42,78 +43,96 @@ typedef struct {
 VQ_Texture* framebuffer;
 pvr_ptr_t texture;
 
-SimplexNoise simplexNoise(1.0f, 0.85f, 2.0f, 0.5f);
+void updateTexture() {
+	unsigned char* pTexture = (unsigned char*)&(framebuffer -> texture);
+
+	int yIndex;
+	int xIndex;
+	
+	for (yIndex = FRAMEBUFFER_HEIGHT; yIndex > 1; yIndex--) {
+		for (xIndex = 0; xIndex < FRAMEBUFFER_WIDTH; xIndex++) {
+			int framebufferIndex = yIndex * FRAMEBUFFER_WIDTH + xIndex;
+			float accumulator = pTexture[(yIndex - 1) * FRAMEBUFFER_WIDTH + (xIndex - 1 + FRAMEBUFFER_WIDTH) % FRAMEBUFFER_WIDTH] +
+				pTexture[(yIndex - 1) * FRAMEBUFFER_WIDTH + (xIndex + FRAMEBUFFER_WIDTH) % FRAMEBUFFER_WIDTH] +
+				pTexture[(yIndex - 1) * FRAMEBUFFER_WIDTH + (xIndex + 1 + FRAMEBUFFER_WIDTH) % FRAMEBUFFER_WIDTH] +
+				pTexture[(yIndex - 2) * FRAMEBUFFER_WIDTH + (xIndex + FRAMEBUFFER_WIDTH) % FRAMEBUFFER_WIDTH];
+			pTexture[framebufferIndex] = (unsigned char)floor(accumulator / 4.0125f);
+		}
+	}
+
+	pvr_txr_load(framebuffer, texture, sizeof(VQ_Texture));
+
+	yIndex = FRAMEBUFFER_WIDTH;
+	for (xIndex = 0; xIndex < FRAMEBUFFER_WIDTH; xIndex++) {
+		pTexture[yIndex + xIndex] = rand() % 255;
+	}
+}
+
+float hue2rgb(float p, float q, float t) {
+	if(t < 0.0f) t += 1.0f;
+	if(t > 1.0f) t -= 1.0f;
+	if(t < 1.0f/6.0f) return p + (q - p) * 6.0f * t;
+	if(t < 1.0f/2.0f) return q;
+	if(t < 2.0f/3.0f) return p + (q - p) * (2.0f/3.0f - t) * 6.0f;
+	return p;
+}
+
+uint16 hslToRgb(float hue, float saturation, float luminance) {
+	float r,g,b;
+
+    if (saturation == 0.0f){
+        r = g = b = luminance; // achromatic
+    } else {
+
+        float q = luminance < 0.5f ? luminance * (1.0f + saturation) : luminance + saturation - luminance * saturation;
+        float p = 2.0f * luminance - q;
+        r = hue2rgb(p, q, hue + 1.0f/3.0f);
+        g = hue2rgb(p, q, hue);
+        b = hue2rgb(p, q, hue - 1.0f/3.0f);
+    }
+
+	uint16 red = round(r * 255);
+	uint16 green = round(g * 255);
+	uint16 blue = round(b * 255);
+
+	return (red >> 3) << 11 | 
+	       (green >> 2) << 5 | 
+		   (blue >> 3);
+} 
 
 void initTexture() {
 	framebuffer = (VQ_Texture*)memalign(64, sizeof(VQ_Texture));
 
-	//This creates a greyscale codebook (4 pixels of the same color, for each level of grey)
+	//This creates a greyscale codebook (4 pixels of the same color, for each level of red)
 	//to use as a palette for this texture
 	unsigned char* codebook = (unsigned char*)&(framebuffer -> codebook);
 	uint16* codebookEntry = (uint16*)codebook;
 	uint32 codebookIdx;
 	for (codebookIdx = 0; codebookIdx < 256; codebookIdx++) {
-		uint16 codebookValue = (((codebookIdx >> 3) << 11) |
-		                        ((codebookIdx >> 2) << 5) |
-		                        (codebookIdx >> 3));
+		float hue = codebookIdx * 0.54f / 255.0f;
+		float saturation = 1.0f;
+		float luminance = (codebookIdx < 128) ? (codebookIdx * 2.0 / 255.0f) : 1.0f;
+		uint16 codebookValue = hslToRgb(hue, saturation, luminance);
 		*codebookEntry++ = codebookValue;
 		*codebookEntry++ = codebookValue;
 		*codebookEntry++ = codebookValue;
 		*codebookEntry++ = codebookValue;
 	}
 
-	//Start with color bands
+	// Initialize texture to 0's
 	unsigned char* textureData = (unsigned char*)&(framebuffer -> texture);
-	int textureIdx;
-	for (textureIdx = 0; textureIdx < FRAMEBUFFER_PIXELS; textureIdx++) {
-		textureData[textureIdx] = textureIdx % FRAMEBUFFER_WIDTH;
+	memset(textureData, FRAMEBUFFER_PIXELS, 1);
+	int xIndex;
+	int yIndex = FRAMEBUFFER_WIDTH;
+	// Randomize the 2nd row from the bottom
+	for (xIndex = 0; xIndex < FRAMEBUFFER_WIDTH; xIndex++) {
+		textureData[xIndex] = rand() % 255;		
+		textureData[yIndex + xIndex] = rand() % 255;
 	}
 
 	texture = pvr_mem_malloc(sizeof(VQ_Texture));
-	pvr_txr_load(framebuffer, texture, sizeof(VQ_Texture));
-}
 
-float xVal = 0.0f;
-
-void updateTexture() {
-	unsigned char* pTexture = (unsigned char*)&(framebuffer -> texture);
-
-	if (xVal == 0.0f) {
-		for (int y = 0; y < FRAMEBUFFER_HEIGHT; y++) {
-			for (int x = 0; x < FRAMEBUFFER_WIDTH; x++) {
-				float pVal = simplexNoise.fractal(5, xVal + ((float)x / (float)FRAMEBUFFER_WIDTH) + 0.5f, ((float)y / (float)FRAMEBUFFER_WIDTH) + 0.5f);
-				if ((pVal > 0.0f) && (pVal < 1.0f)) {
-					*pTexture = (unsigned char)(pVal * 255.0f);
-				} else if (pVal >= 1.0f) {
-					*pTexture = 255;
-				} else {
-					*pTexture = 0;
-				}
-				pTexture++;
-			}
-		}		
-	} else {
-		for (int y = 0; y < FRAMEBUFFER_HEIGHT; y++) {
-			for (int x = 0; x < (FRAMEBUFFER_WIDTH - 1); x++) {
-				*pTexture = pTexture[1];
-				pTexture++;
-			}
-
-			float pVal = simplexNoise.fractal(5, xVal + ((float)(FRAMEBUFFER_WIDTH - 1) / (float)FRAMEBUFFER_WIDTH) + 0.5f, ((float)y / (float)FRAMEBUFFER_WIDTH) + 0.5f);
-			if ((pVal > 0.0f) && (pVal < 1.0f)) {
-				*pTexture = (unsigned char)(pVal * 255.0f);
-			} else if (pVal >= 1.0f) {
-				*pTexture = 255;
-			} else {
-				*pTexture = 0;
-			}
-			pTexture++;
-		}				
-	}
-
-	pvr_txr_load(framebuffer, texture, sizeof(VQ_Texture));
-
-	xVal += (1.0f / (float)FRAMEBUFFER_WIDTH);
+	updateTexture();
 }
 
 void freeTexture() {
@@ -143,24 +162,24 @@ void drawScreen() {
 	my_vertex.y = 480.0f;
 	my_vertex.z = 1.0f;
 	my_vertex.u = 0.0f;
-	my_vertex.v = 1.0f;
+	my_vertex.v = 0.02f;
 	my_vertex.argb = 0xFFFFFFFF;
 	my_vertex.oargb = 0;
 	pvr_prim(&my_vertex, sizeof(my_vertex));
 
 	my_vertex.y = 0.0f;
-	my_vertex.v = 0.0f;
+	my_vertex.v = 1.0f;
 	pvr_prim(&my_vertex, sizeof(my_vertex));
 
 	my_vertex.x = 640.0f;
 	my_vertex.y = 480.0f;
 	my_vertex.u = 1.0f;
-	my_vertex.v = 1.0f;
+	my_vertex.v = 0.02f;
 	pvr_prim(&my_vertex, sizeof(my_vertex));
 
 	my_vertex.flags = PVR_CMD_VERTEX_EOL;
 	my_vertex.y = 0.0f;
-	my_vertex.v = 0.0f;
+	my_vertex.v = 1.0f;
 	pvr_prim(&my_vertex, sizeof(my_vertex));
 
 	pvr_list_finish();
